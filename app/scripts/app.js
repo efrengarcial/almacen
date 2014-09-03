a302e7e5ee73daba93965d0e7ab7daefac713bef
efrengarcial <efren.gl@gmail.com>
2014-09-02 22:54:22 -0500

    Desa

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,6 +25,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/productos', {
+        templateUrl: 'views/productos.html',
+        controller: 'ProductosCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
