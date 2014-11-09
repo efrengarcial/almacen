122f37c12558bf89addc4e198c02d8a5dc0cca56
Dario <dariodeath@gmail.com>
2014-11-08 23:34:50 -0500

    Agregado archivo ordenCompra.html

---
+++ app/scripts/controllers/ordenCompra.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name almacenApp.controller:OrdenCompraCtrl
+ * @description
+ * # OrdenCompraCtrl
+ * Controller of the almacenApp
+ */
+angular.module('almacenApp')
+    .controller('OrdenCompraCtrl', function($scope, $log) {
+        $log.debug('Iniciando orden compra...');
+
+    });
