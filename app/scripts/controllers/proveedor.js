1f737e7608b9d2303f03cc9cb4d4fd4b984259bd
Dario <dariodeath@gmail.com>
2014-12-05 00:22:57 -0500

    Validaciones Formulario Proveedor

--- app/scripts/controllers/proveedor.js
+++ app/scripts/controllers/proveedor.js
@@ -11,11 +11,44 @@ angular.module('almacenApp')
     .controller('ProveedorCtrl', function($scope, $log) {
         $log.debug('Iniciando orden proveedores...');
 
+
+        $scope.submit = function(isValid) {
+            $log.debug(isValid);
+            if (isValid) {
+                alert('Ok');
+            }
+
+        };
+
+        $scope.interacted = function(field) {
+            return $scope.submitted || field.$dirty;
+        };
+
         $scope.showMessage = function() {
             $('.required-icon, .required-combo-icon').tooltip({
                 placement: 'left',
                 title: 'Campo requerido'
             });
-        };        
+        };
 
+    }).directive('numbersOnly', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, element, attrs, modelCtrl) {
+                modelCtrl.$parsers.push(function(inputValue) {
+
+                    if (inputValue === undefined) {
+                        return '';
+                    }
+
+                    var transformedInput = inputValue.replace(/[^0-9]/g, '');
+                    if (transformedInput !== inputValue) {
+                        modelCtrl.$setViewValue(transformedInput);
+                        modelCtrl.$render();
+                    }
+
+                    return inputValue;
+                });
+            }
+        };
     });
