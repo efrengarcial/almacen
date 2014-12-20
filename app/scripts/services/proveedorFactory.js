f2ca1bdefaf1245dec81fe646ea56dc95a1fe362
Dario <dariodeath@gmail.com>
2014-12-20 00:14:59 -0500

    Comunicacion backend proveedores

---
+++ app/scripts/services/proveedorFactory.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name almacenApp.listasFactory
+ * @description
+ * # listasFactory
+ * Factory in the almacenApp.
+ */
+
+angular
+    .module('almacenApp')
+    .factory('proveedorFactory', ['Restangular', 'apiFactory', 'WS',
+        function(Restangular, apiFactory, WS) {
+            return {
+                saveProveedor: function(proveedor) {
+                    return apiFactory.all(WS.URI_SAVE_PROVEEDOR).post(proveedor);
+                },
+                query: function(search) {
+                    return apiFactory.all(WS.URI_QUERY_PROVEEDOR).one(search).
+                    getList().then(function(proveedores) {
+                        return proveedores;
+                    });
+                },
+            };
+        }
+    ]);
